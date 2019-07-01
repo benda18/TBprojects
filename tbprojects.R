e896df3e73204fe60d2c42ccc8057c40413575c2
Tim Bender <bendertj@gmail.com>
2019-07-01 11:36:35 -0400

    Update tbprojects.R

--- tbprojects.R
+++ tbprojects.R
@@ -5,7 +5,8 @@ library("ggrepel")
 
 
 #vars----
-wrap_len <- 30
+wrap_len <- 21
+date.updated <- ymd(20190701)
 
 #functions----
 fun_project <- function(project.name, last.updated,
@@ -38,28 +39,41 @@ fun_event <- function(event.name, project.name,
 
 #data creation----
 cityplanning.prj <- rbind(fun_project("2030 Comp Plan Update", 
-                                      last.updated = ymd(20190701), 
+                                      last.updated = date.updated, 
                                       "citywide", "active", 
                                       start.date = ymd(20190312), 
                                       end.date = ymd(20190903), 
                                       url = NA), 
                           fun_project("Raleigh BRT: Equitable Development Around Transit", 
-                                      last.updated = ymd(20190701), 
+                                      last.updated = date.updated, 
                                       "citywide", "active", 
-                                      start.date = NA, end.date = NA, 
+                                      start.date = ymd(20190606), 
+                                      end.date = ymd(20191231), 
                                       url = NA))
-cityplanning.evnt <- rbind(fun_event("Ask a Planner", "2030 Comp Plan Update", 
-                                     "RMB 237", ymd(20190702)))
+cityplanning.evnt <- rbind(fun_event("Ask a Planner", 
+                                     "2030 Comp Plan Update", 
+                                     "RMB 237", ymd(20190702)), 
+                           fun_event("Public Hearing CP-2C-19", 
+                                     "2030 Comp Plan Update", 
+                                     "Council Chambers", ymd(20190702)), 
+                           fun_event("Public Hearing CP-2B-19", 
+                                     "2030 Comp Plan Update", 
+                                     "Council Chambers", ymd(20190903)), 
+                           fun_event("Begin: Station Area Planning", 
+                                     "Raleigh BRT: Equitable Development Around Transit", 
+                                     NA, ymd(20200101)))
 
 #tidy wrap long text strings
 cityplanning.evnt$project.name <- str_wrap(cityplanning.evnt$project.name, width = wrap_len)
 cityplanning.prj$project.name  <- str_wrap(cityplanning.prj$project.name, width = wrap_len) 
 
+#tidy events
+cityplanning.evnt <- inner_join(cityplanning.evnt, cityplanning.prj[!colnames(cityplanning.prj) %in% c("start.date", "end.date")])
 
 #plot----
 ggplot() + 
   geom_vline(xintercept = Sys.Date(), color = "black", 
-             linetype = 2232)+
+             linetype = 2)+
   geom_segment(data = cityplanning.prj, 
                size = 2.0,
                aes(x = start.date, xend = end.date, 
@@ -74,24 +88,28 @@ ggplot() +
                  color = project.name)) +
   geom_label_repel(data = cityplanning.evnt, 
                    fill = "white", 
+                   size = 3.25,
                    min.segment.length = 0, point.padding = unit(0.325, "inches"),
                    arrow = arrow(angle = 20, type = "closed", unit(0.075, "inches")),
                    direction = "y", 
                    aes(x = event.date, 
                        y = project.name, 
                        label = event.name)) +
-  scale_x_date(name = "Date") +
+  scale_x_date(name = "Date", 
+               date_breaks = "2 month", date_labels = "%b\n%Y", 
+               date_minor_breaks = "1 month") +
   theme_linedraw() + 
-  theme(strip.text.y = element_text(angle = 180), 
-        axis.text.y = element_blank(), 
-        axis.ticks.y = element_blank(), 
-        axis.title.y = element_blank(),
+  theme(strip.text.y = element_text(angle = 0),
+        axis.text.x = element_text(angle = 0, hjust = 0.5, vjust = 0.5),
+        #axis.text.y = element_blank(), 
+        #axis.ticks.y = element_blank(), 
+        axis.title = element_blank(),
         legend.position = "none") +
-  facet_grid(project.name~.,  
+  facet_grid(project.status~.,  
              #margins = "project.name",
-             space = "free_y", scales = "free_y", 
-             switch = "y") +
-  labs(title = "title", 
+             space = "free_y", scales = "free_y") +
+  labs(title = "Raleigh City Planning Ongoing Projects", 
+       subtitle = paste("Updated:", format(date.updated, format = "%B %d, %Y")),
        color = "Project")
 
 # #import city calendar from api----
